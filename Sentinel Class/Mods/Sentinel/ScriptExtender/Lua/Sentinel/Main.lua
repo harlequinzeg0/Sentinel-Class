d93d089ff99bbe6aa4941cc7b2dc33e9955f5ec7
harlequinzeg0 <j.wardftw@gmail.com>
2023-10-09 07:00:06 -0700

    remove unneeded hello print

--- Sentinel Class/Mods/Sentinel/ScriptExtender/Lua/Sentinel/Main.lua
+++ Sentinel Class/Mods/Sentinel/ScriptExtender/Lua/Sentinel/Main.lua
@@ -1,6 +1,5 @@
 -- Osiris event listeners
 Ext.Osiris.RegisterListener('CastedSpell', 5, 'after', function(caster, spell, spellType, spellElement, storyActionID)
-    print("hello")
 	if spell == 'Shout_DualShield' then
 		shield = Osi.GetEquippedShield(caster)
 		Ext.Entity.Get(shield).Equipable.Slot = 'MeleeMainHand'
